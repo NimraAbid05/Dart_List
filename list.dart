600ee49f5787d6d579faf85fb494f53cad844a25
NimraAbid05 <nimram214@gmail.com>
2022-10-16 19:19:10 +0500

    list

---
+++ list.dart
@@ -0,0 +1,16 @@
+void main() {
+  List n = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+  //index of list=[0,1,2,3,4,5,6,7,8]
+  //index of list=[0,1,2,3,4,5,6,7,8]
+  print(n[3]);
+  //isEmpty check list is empty
+  print(n.isEmpty);
+  // isNotEmpty check list is not empty
+  print(n.isNotEmpty);
+  //.last print last element
+  print(n.last);
+  //.first print first element
+  print(n.first);
+  //find & print element 7
+  print(n[6]);
+}
